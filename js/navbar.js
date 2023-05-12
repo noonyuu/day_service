8cabf44e3c588cee8ab888c6c329b5910b581481
清水 <noon3m.op.sh@gmail.com>
2023-05-13 04:31:19 +0900

    [add]navbarを動かす用のjsファイル

---
+++ js/navbar.js
@@ -0,0 +1,3 @@
+document.querySelector('[data-collapse-toggle="navbar-default"]').addEventListener("click", function () {
+  document.querySelector("#navbar-default").classList.toggle("hidden");
+});
