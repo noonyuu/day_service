1c95ba06b6ea1ae369142cc91c7fa73e13e1a1a0
清水 <noon3m.op.sh@gmail.com>
2023-05-15 12:04:14 +0900

    [update]微調整

--- account_info.php
+++ account_info.php
@@ -21,6 +21,7 @@ require_once dirname(__FILE__) . '/function/select.php';
   <link href="./css/output.css" rel="stylesheet">
   <!-- css -->
   <link href="./css/my_style.css" rel="stylesheet">
+  <!-- <link href="./css/reset.css" rel="stylesheet"> -->
   <!-- icon -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
